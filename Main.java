69424e0adda3fb2d99044b55c76ebb4fd720822f
Edwin <ebustilman1@educacion.navarra.es>
2025-11-14 18:14:54 +0100

    Añadido mensaje de despedida

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+y
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo, soy  Edwin");
-    }
+	  System.out.println("Que tengas buen dia");
+		    }
 }
 
 Version inicial del proyecto saludador
